6ec6a7d83fe1c0baf370857afd34d8fc52c3fda5
gradel <gerald@hien-online.de>
2015-07-19 22:04:59 +0200

    remove import dependent Category base class creation

--- mezzanine_blocks/category.py
+++ mezzanine_blocks/category.py
@@ -1,29 +1,12 @@
-from django.db import models
-from django.core.cache import cache
 from django.utils.translation import ugettext_lazy as _
 from mezzanine.core.models import Slugged
 
 
-try:
-    from mptt.models import MPTTModel, TreeForeignKey
-
-    class BaseBlockCategory(MPTTModel, Slugged):
-        """Base Category (MPTT tree).
-        """
-        parent = TreeForeignKey('self', null=True, blank=True, related_name='child')
-
-        class Meta:
-            abstract = True
-
-        class MPTTMeta:
-            order_insertion_by = ['title']
-
-except ImportError:
-    class BaseBlockCategory(Slugged):
-        """Base Category
-        """
-        class Meta:
-            abstract = True
+class BaseBlockCategory(Slugged):
+    """Base Category
+    """
+    class Meta:
+        abstract = True
 
 
 class BlockCategory(BaseBlockCategory):
